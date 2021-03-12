eed8bb723f7a9502a0cce53b4996aa77aac04c86
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-03-12 16:59:45 -0300

    first commit

---
+++ src/main/java/com/javahackerrank/JavaHackerrankApplication.java
@@ -0,0 +1,13 @@
+package com.javahackerrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHackerrankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHackerrankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javahackerrank/JavaHackerrankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javahackerrank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHackerrankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
